732a28105408441bfaf248b5a0d8e2f85ba459d3
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-12-21 10:13:08 +0600

    Update test3.vue

--- test3.vue
+++ test3.vue
@@ -1,1 +1,62 @@
 
+<script setup lang="ts">
+  import { ref } from 'vue'
+  defineProps({
+    type: {
+      type: String,
+      required: false,
+      default: 'text',
+    },
+    placeholder: {
+      type: String,
+      required: false,
+    },
+    required: {
+      type: Boolean,
+      required: false,
+    },
+    minLength: {
+      type: Number,
+      required: false,
+    },
+    maxLength: {
+      type: Number,
+      required: false,
+    },
+    disabled: {
+      type: Boolean,
+      required: false,
+    },
+    readonly: {
+      type: Boolean,
+      required: false,
+    },
+    label: {
+      type: String,
+      required: false,
+      default: '',
+    },
+  })
+  const emits = defineEmits(['onChange'])
+
+  const value = ref('')
+
+  const onChange = () => {
+    emits('onChange', { value })
+  }
+</script>
+<template>
+  <input
+    class="alser-ui-library-input"
+    v-model="value"
+    :type="type"
+    :placeholder="placeholder"
+    autofocus
+    :required="required"
+    :minLength="minLength"
+    :maxlength="maxLength"
+    :disabled="disabled"
+    :readonly="readonly"
+    @input="onChange"
+  />
+</template>
