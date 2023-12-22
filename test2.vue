0a40d252102d798613eeb84ba1397094c40c9857
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-12-22 12:28:05 +0600

    Update test2.vue

--- test2.vue
+++ test2.vue
@@ -1,6 +1,9 @@
-<div 
-  class="input-wrapper"
-  :style="{borderColor: errorText ? 'red' : 'initial'}"
->
-  <!-- Содержимое вашего div -->
-</div>
+DocumentationOverview.vue:35 [Vue warn]: Invalid prop: type check failed for prop "value". Expected String with value "34", got Number with value 34. 
+  at <BaseRadio name="ghbd" value=34 class="error" > 
+  at <InputsDocs> 
+  at <DocumentationOverview> 
+  at <MainContainer> 
+  at <App>
+
+
+в чем проблема?
