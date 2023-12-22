ff89b86d7561382be1362ee325ebc6265bebecf3
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-12-22 18:26:06 +0600

    Update test3.vue

--- test3.vue
+++ test3.vue
@@ -1,7 +1,6 @@
-  .input-wrapper:hover {
+.input-wrapper:not(:has(input:disabled)):hover {
     border-radius: var(--radius-2xl, 16px);
     border: 1px solid var(--border-secondary, #d8dcd8);
     background: var(--colors-palette-basic-white, #fff);
-    /* focus-rings/ring-brand */
     box-shadow: 0px 0px 0px 4px rgba(70, 167, 88, 0.24);
-  }
+}
