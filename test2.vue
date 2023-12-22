c37e2577753f80ed37e82b32272d563effe8c8f3
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-12-22 15:33:21 +0600

    Update test2.vue

--- test2.vue
+++ test2.vue
@@ -1,19 +1,23 @@
-  .input-wrapper_error {
-    border-radius: var(--radius-2xl, 16px) !important;
-    border: 1px solid var(--colors-border-error, #f3b0a2) !important;
-    background: var(--colors-bg-primary, #fff) !important;
-    box-shadow: 0px 1px 2px 0px rgba(16, 40, 33, 0.05) !important;
-  }
-  .input-wrapper_error:focus,
-  .input-wrapper_error:hover,
-  .input-wrapper_error:active {
+/* Стили для состояния hover */
+.input-wrapper_error:hover {
     border-radius: var(--radius-2xl, 16px) !important;
     border: 1px solid var(--colors-border-secondary, #d8dcd8) !important;
     background: var(--colors-palette-basic-white, #fff) !important;
     box-shadow: 0px 0px 0px 4px rgba(229, 77, 46, 0.24) !important;
-  }
+}
 
-  тут я применяю стили для инпута при hover focus и active
-но работает корректно только hover 
+/* Стили для состояния focus */
+.input-wrapper_error:focus {
+    border-radius: var(--radius-2xl, 16px) !important;
+    border: 1px solid var(--colors-border-secondary, #d8dcd8) !important;
+    background: var(--colors-palette-basic-white, #fff) !important;
+    box-shadow: 0px 0px 0px 4px rgba(229, 77, 46, 0.24) !important;
+}
 
-почему?
+/* Стили для состояния active */
+.input-wrapper_error:active {
+    border-radius: var(--radius-2xl, 16px) !important;
+    border: 1px solid var(--colors-border-secondary, #d8dcd8) !important;
+    background: var(--colors-palette-basic-white, #fff) !important;
+    box-shadow: 0px 0px 0px 4px rgba(229, 77, 46, 0.24) !important;
+}
