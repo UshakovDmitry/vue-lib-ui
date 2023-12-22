5e117100234ceb1d7dfdfe2b69baf99ec8b1d6a3
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-12-22 15:49:59 +0600

    Update test2.vue

--- test2.vue
+++ test2.vue
@@ -1,21 +1,10 @@
 <template>
   <div class="alser-ui-library-field">
-    <label v-if="label" class="label">
-      <p class="lable__text">
-        {{ label }}
-      </p>
-      <p v-if="required" class="label__mark">*</p>
-    </label>
+    <!-- ... ваш код ... -->
 
-    <div
-      class="input-wrapper"
-      :class="{
-        'input-wrapper_error': errorText,
-        'input-wrapper_disabled': disabled,
-      }"
-    >
-
+    <div class="input-wrapper" :class="{ 'input-wrapper_error': errorText, 'input-wrapper_disabled': disabled }">
       <BaseInput
+        v-model="inputValue"
         :type="type"
         :placeholder="placeholder"
         :required="required"
@@ -24,68 +13,36 @@
         :disabled="disabled"
         :readonly="readonly"
         :icon="icon"
-        @onChange="emits('onChange', $event)"
+        @onChange="onChange"
       ></BaseInput>
-
+      
+      <!-- Иконка крестика, отображается, когда в inputValue есть текст -->
+      <IconComponent v-if="inputValue" class="clear-icon" @click="clearInput" />
     </div>
-    <span class="error-text">{{ errorText }}</span>
+
+    <!-- ... ваш код ... -->
   </div>
 </template>
 
 <script setup lang="ts">
+  import { ref } from 'vue'
   import BaseInput from '@/components/BaseInput.vue'
-  import SearchIcon from '@/docs/IconCollection/SearchIcon.vue'
-  import PersonOutline from '@/docs/IconCollection/PersonOutline.vue'
   import IconComponent from '@/components/Icon.vue'
-  defineProps({
-    placeholder: {
-      type: String,
-      required: false,
-    },
-    required: {
-      type: Boolean,
-      required: false,
-      default: false,
-    },
-    minLength: {
-      type: Number,
-      required: false,
-    },
-    maxLength: {
-      type: Number,
-      required: false,
-    },
-    disabled: {
-      type: Boolean,
-      required: false,
-    },
-    readonly: {
-      type: Boolean,
-      required: false,
-    },
-    label: {
-      type: String,
-      required: false,
-      default: '',
-    },
-    icon: {
-      type: String,
-      required: false,
-      default: '',
-    },
-    type: {
-      type: String,
-      required: false,
-      default: 'text',
-    },
-    errorText: {
-      type: String,
-      required: false,
-      default: '',
-    },
-  })
 
+  // Определение пропсов ...
+  
   const emits = defineEmits(['onChange'])
+  const inputValue = ref('') // Реактивное свойство для значения инпута
+
+  // Обработчик события изменения
+  const onChange = (event) => {
+    emits('onChange', event)
+  }
+
+  // Функция для очистки инпута
+  const clearInput = () => {
+    inputValue.value = ''
+  }
 </script>
 
-BaseInput это и есть мой input
+<!-- Добавьте стили для .clear-icon по вашему усмотрению -->
