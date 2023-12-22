e82c65f4e04b076ad1baaa6ad5d9ba139bf7913c
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-12-22 11:02:30 +0600

    Update test2.vue

--- test2.vue
+++ test2.vue
@@ -1,4 +1,6 @@
-  <div 
-    class="input-wrapper"
-    :style="{'b': errorText}"
-    >
+<div 
+  class="input-wrapper"
+  :style="{borderColor: errorText ? 'red' : 'initial'}"
+>
+  <!-- Содержимое вашего div -->
+</div>
