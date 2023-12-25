5ebf22352b5d4af70af72d9f42f73eb9143e9c17
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-12-25 14:44:40 +0600

    Update test3.vue

--- test3.vue
+++ test3.vue
@@ -1,92 +1,70 @@
-import { mount } from '@vue/test-utils';
-import PasswordField from '@/components/PasswordField.vue';
-import { describe, expect, test, afterEach, beforeEach } from "vitest";
+<template>
+  <div class="snippet-sourse">
+    <p class="snippet-source-title">{{ source }}</p>
+    <figure class="hightlight">
+      <pre>
+        <code class="language-html" >
+          <slot ></slot>
+        </code>
+      </pre>
+    </figure>
+  </div>
+</template>
 
-describe('PasswordField', () => {
-    let wrapper;
+<script setup lang="ts">
 
-    beforeEach(() => {
-        wrapper = mount(PasswordField, {
-            props: {
-                label: 'Password',
-                placeholder: 'Enter password',
-                errorText: '',
-                hintText: 'Password should be strong',
-            }
-        });
-    });
+  defineProps({
+    source: {
+      type: String,
+      default: 'HTML',
+      required: false,
+    },
+  })
+</script>
 
-    afterEach(() => {
-        wrapper.unmount();
-    });
+<style scoped>
+  .snippet-sourse {
+    background-color: var(--gray-light);
+    border-radius: 0.5rem;
+    margin-bottom: 1.5rem;
+    width: 50%;
+  }
 
-    test('Компонент существует', () => {
-        expect(wrapper.exists()).toBe(true);
-    });
+  .snippet-source-title {
+    background-color: var(--dark);
+    color: var(--secondary-brand);
+    padding: 0.5rem;
+    border-top-left-radius: 0.5rem;
+    border-top-right-radius: 0.5rem;
+    display: flex;
+    width: 70px;
+    justify-content: center;
+  }
 
-    test('Компонент отображает правильные пропсы', () => {
-        expect(wrapper.props('label')).toBe('Password');
-        expect(wrapper.props('placeholder')).toBe('Enter password');
-        expect(wrapper.props('errorText')).toBe('');
-        expect(wrapper.props('hintText')).toBe('Password should be strong');
-    });
+  .hightlight {
+    font-size: 1em;
+    background-color: var(--dark);
+    border-radius: 6px;
+    border-top-left-radius: 0;
+    color: var(--white);
+    width: 100%;
+  }
 
-    test('Клик по иконке видимости меняет тип поля ввода', async () => {
-        const visibilityIcon = wrapper.find('icon-component'); // Уточните селектор, если нужно
-        await visibilityIcon.trigger('click');
-        expect(wrapper.find('input').element.type).toBe('text');
-        await visibilityIcon.trigger('click');
-        expect(wrapper.find('input').element.type).toBe('password');
-    });
+  .highlight pre {
+    background-color: transparent;
+    color: currentColor;
+    font-size: 1em;
+    line-height: 1.375;
+    margin: 0 !important;
+    padding: 1.25em 1.5em;
+    white-space: pre;
+    word-break: break-word;
+    font-family: monospace;
+  }
 
-    test('Проверка отображения иконок видимости и скрытия видимости', async () => {
-        const visibilityIcon = wrapper.find('icon-component'); // Уточните селектор, если нужно
-        expect(wrapper.findComponent({ name: 'VisibilityIcon' }).exists()).toBe(true);
-        await visibilityIcon.trigger('click');
-        expect(wrapper.findComponent({ name: 'VisibilityOffIcon' }).exists()).toBe(true);
-    });
-
-    // Добавьте другие тесты по необходимости
-});
-
-
-
- FAIL  src/components/PasswordField.test.ts > PasswordField > Клик по иконке видимости меняет тип поля ввода
-Error: Cannot call trigger on an empty DOMWrapper.
- ❯ Object.get node_modules/@vue/test-utils/dist/vue-test-utils.esm-bundler.mjs:1525:27
- ❯ src/components/PasswordField.test.ts:36:30
-     34|     test('Клик по иконке видимости меняет тип поля ввода', async () => {
-     35|         const visibilityIcon = wrapper.find('icon-component'); // Уточните селектор, если нужно
-     36|         await visibilityIcon.trigger('click');
-       |                              ^
-     37|         expect(wrapper.find('input').element.type).toBe('text');
-     38|         await visibilityIcon.trigger('click');
-
-⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯[2/3]⎯
- FAIL  src/components/PasswordField.test.ts > PasswordField > Проверка отображения иконок видимости и скрытия видимости
-AssertionError: expected false to be true // Object.is equality
-
-- Expected
-+ Received
-
-- true
-+ false
-
- ❯ src/components/PasswordField.test.ts:44:76
-     42|     test('Проверка отображения иконок видимости и скрытия видимости', async () => {
-     43|         const visibilityIcon = wrapper.find('icon-component'); // Уточните селектор, если нужно
-     44|         expect(wrapper.findComponent({ name: 'VisibilityIcon' }).exists()).toBe(true);
-       |                                                                            ^
-     45|         await visibilityIcon.trigger('click');
-     46|         expect(wrapper.findComponent({ name: 'VisibilityOffIcon' }).exists()).toBe(true);
-
-⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯[3/3]⎯
- Test Files  2 failed | 18 passed (20)
-      Tests  2 failed | 59 passed (61)
-   Start at  12:02:09
-   Duration  2.62s (transform 553ms, setup 2ms, collect 2.25s, tests 775ms, environment 5.20s, prepare 3.00s)
-
-
- FAIL  Tests failed. Watching for file changes...
-       press h to show help, press q to quit
-
+  @media screen and (max-width: 1279px) {
+    .snippet-sourse {
+      width: 100%;
+    }
+  }
+</style>
