529dfa9432660d31df9fa0c0a4ab8663aa4dd950
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-12-25 14:59:00 +0600

    Update test3.vue

--- test3.vue
+++ test3.vue
@@ -1,76 +1,49 @@
-я не хочу использовать библиотеки 
-вот что я передаю в компонент 
+<script setup lang="ts">
+import { computed } from 'vue';
 
-    <DocsContentSnippetSource :source="'JS'">
-      {{ textFieldProps }}</DocsContentSnippetSource
-    >
+defineProps({
+  source: {
+    type: String,
+    default: 'HTML',
+    required: false,
+  },
+});
+
+const propsToHighlight = ['type', 'placeholder', 'required', 'minLength', 'maxLength', 'disabled', 'readonly', 'label', 'icon', 'errorText', 'hintText'];
+
+const highlightedCode = computed(() => {
+  let code = props.textFieldProps;
+  propsToHighlight.forEach(prop => {
+    const regex = new RegExp(`(${prop})`, 'g');
+    code = code.replace(regex, '<span class="highlight-prop">$1</span>');
+  });
+  return code;
+});
+</script>
 
 
-  const textFieldProps = ` 
-     defineProps({ 
 
-      type: { 
-        type: String,
-        required: false,
-        default: 'text',
-      },
+<template>
+  <div class="snippet-sourse">
+    <p class="snippet-source-title">{{ source }}</p>
+    <figure class="hightlight">
+      <pre>
+        <code class="language-html" v-html="highlightedCode">
+          <!-- Слот убран, так как мы используем v-html -->
+        </code>
+      </pre>
+    </figure>
+  </div>
+</template>
 
-      placeholder: {         
-        type: String, 
-        required: false, 
-      }, 
 
-      required: {
-        type: Boolean, 
-        required: false, 
-        default: false, 
-      }, 
-        
-      minLength: { 
-        type:Number,
-         required: false,
-       },
-       
-      maxLength: { 
-        type: Number,
-        required: false, 
-      },
+<style scoped>
+  /* ... предыдущие стили ... */
 
-      disabled: {
-         type: Boolean,
-         required: false, 
-        }, 
-        
-      readonly: { 
-        type: Boolean,
-        required: false,
-      }, 
-      
-      label: {
-         type: String,
-         required: false,
-         default: '',
-      },
-      
-      icon: {
-        type: String,
-        required: false,
-        default: '', 
-      },
-        
-      errorText: { 
-        type: String,
-        required: false,
-        default: '', 
-      }, 
-      
-      hintText: { 
-        type: String,
-        required: false,
-        default: '',
-      },
-           
-      })`
+  .highlight-prop {
+    color: #ff8c00; /* Оранжевый цвет для выделения */
+    font-weight: bold; /* Делаем текст жирным */
+  }
 
-я хочу выделить название пропсов type ,placeholder , required,minLength и тд
-эти слова я хочу выделить оранжевым цветом
+  /* ... остальные стили ... */
+</style>
