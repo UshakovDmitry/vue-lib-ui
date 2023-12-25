1a9372d75c1bee2e6d2c7ee1fb5b22ba5f849f3c
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-12-25 15:41:43 +0600

    Update test3.vue

--- test3.vue
+++ test3.vue
@@ -1,54 +1,204 @@
-import { mount } from '@vue/test-utils';
-import SearchField from '@/components/SearchField.vue';
-import { describe, expect, test, afterEach, beforeEach } from 'vitest';
+import { mount, VueWrapper } from '@vue/test-utils';
+import { describe, expect, test, afterEach, beforeEach, vi } from "vitest";
+import TextField from '@/components/TextField.vue';
 
-describe('SearchField', () => {
-  let wrapper;
-
-  beforeEach(() => {
-    wrapper = mount(SearchField, {
-      props: {
-        label: 'Search',
-        placeholder: 'Enter search term',
-        errorText: '',
-        hintText: '',
-      },
+describe('TextField', () => {
+    let wrapper: VueWrapper<any>;
+    let TEXTFIELD: VueWrapper<any>;
+    
+    beforeEach(() => {
+        wrapper = mount(TextField, {
+        props: {
+            copyText: 'test',
+        }
+        });
+    
+        TEXTFIELD = wrapper.findComponent(TextField);
     });
-  });
+    
+    afterEach(() => {
+        wrapper.unmount();
+    });
+    
+    test('Компонент существует', () => {
+        expect(wrapper.exists()).toBe(true);
+    });
+    
+    
+    test('Компонент имеет событие onClick', () => {
+        TEXTFIELD.trigger('click');
+    
+        expect(wrapper.emitted()).toHaveProperty('click');
+    });
 
-  afterEach(() => {
-    wrapper.unmount();
-  });
+    test('Компонент имеет событие onCopy', () => {
+        TEXTFIELD.trigger('copy');
+    
+        expect(wrapper.emitted()).toHaveProperty('copy');
+    });
 
-  test('Компонент существует', () => {
-    expect(wrapper.exists()).toBe(true);
-  });
+    test('Компонент имеет событие onCut', () => {
+        TEXTFIELD.trigger('cut');
+    
+        expect(wrapper.emitted()).toHaveProperty('cut');
+    });
 
-  test('Компонент отображает правильные пропсы', () => {
-    expect(wrapper.props('label')).toBe('Search');
-    expect(wrapper.props('placeholder')).toBe('Enter search term');
-    expect(wrapper.props('errorText')).toBe('');
-    expect(wrapper.props('hintText')).toBe('');
-  });
+    test('Компонент имеет событие onPaste', () => {
+        TEXTFIELD.trigger('paste');
+    
+        expect(wrapper.emitted()).toHaveProperty('paste');
+    });
 
-  test('Компонент правильно обрабатывает ввод данных', async () => {
-    const input = wrapper.find('input[type="search"]');
-    await input.setValue('test');
-    expect(input.element.value).toBe('test');
-  });
 
-  test('Компонент правильно обрабатывает состояние disabled', async () => {
-    await wrapper.setProps({ disabled: true });
-    const input = wrapper.find('input[type="search"]');
-    expect(input.attributes('disabled')).toBeDefined();
-  });
+    test('Компонент имеет событие onFocus', () => {
+        TEXTFIELD.trigger('focus');
+    
+        expect(wrapper.emitted()).toHaveProperty('focus');
+    });
 
-  test('Компонент правильно обрабатывает состояние readonly', async () => {
-    await wrapper.setProps({ readonly: true });
-    const input = wrapper.find('input[type="search"]');
-    expect(input.attributes('readonly')).toBeDefined();
-  });
+    test('Компонент имеет событие onBlur', () => {
+        TEXTFIELD.trigger('blur');
+    
+        expect(wrapper.emitted()).toHaveProperty('blur');
+    });
 
-  // Добавьте дополнительные тесты по необходимости
+    test('Компонент имеет событие onInput', () => {
+        TEXTFIELD.trigger('input');
+    
+        expect(wrapper.emitted()).toHaveProperty('input');
+    });
+
+    test('Компонент имеет событие onKeydown', () => {
+        TEXTFIELD.trigger('keydown');
+    
+        expect(wrapper.emitted()).toHaveProperty('keydown');
+    });
+
+    test('Компонент имеет событие onKeypress', () => {
+        TEXTFIELD.trigger('keypress');
+    
+        expect(wrapper.emitted()).toHaveProperty('keypress');
+    });
+
+    test('Компонент имеет событие onKeyup', () => {
+        TEXTFIELD.trigger('keyup');
+    
+        expect(wrapper.emitted()).toHaveProperty('keyup');
+    });
+
+    test('Компонент имеет событие onMousedown', () => {
+        TEXTFIELD.trigger('mousedown');
+    
+        expect(wrapper.emitted()).toHaveProperty('mousedown');
+    });
+
+    test('Компонент имеет событие onMouseenter', () => {
+        TEXTFIELD.trigger('mouseenter');
+    
+        expect(wrapper.emitted()).toHaveProperty('mouseenter');
+    });
+
+    test('Компонент имеет событие onMouseleave', () => {
+        TEXTFIELD.trigger('mouseleave');
+    
+        expect(wrapper.emitted()).toHaveProperty('mouseleave');
+    });
+
+    test('Компонент имеет событие onMousemove', () => {
+        TEXTFIELD.trigger('mousemove');
+    
+        expect(wrapper.emitted()).toHaveProperty('mousemove');
+    });
+
+    test('Компонент имеет событие onMouseout', () => {
+        TEXTFIELD.trigger('mouseout');
+    
+        expect(wrapper.emitted()).toHaveProperty('mouseout');
+    });
+
+    test('Компонент имеет событие onMouseover', () => {
+        TEXTFIELD.trigger('mouseover');
+    
+        expect(wrapper.emitted()).toHaveProperty('mouseover');
+    });
+
+    test('Компонент имеет событие onMouseup', () => {
+        TEXTFIELD.trigger('mouseup');
+    
+        expect(wrapper.emitted()).toHaveProperty('mouseup');
+    });
+
+    test('Компонент имеет событие onWheel', () => {
+        TEXTFIELD.trigger('wheel');
+    
+        expect(wrapper.emitted()).toHaveProperty('wheel');
+    });
+
+    test('Компонент имеет событие onCompositionend', () => {
+        TEXTFIELD.trigger('compositionend');
+    
+        expect(wrapper.emitted()).toHaveProperty('compositionend');
+    });
+
+    test('Компонент имеет событие onCompositionstart', () => {
+        TEXTFIELD.trigger('compositionstart');
+    
+        expect(wrapper.emitted()).toHaveProperty('compositionstart');
+    });
+
+    test('Компонент имеет событие onCompositionupdate', () => {
+        TEXTFIELD.trigger('compositionupdate');
+    
+        expect(wrapper.emitted()).toHaveProperty('compositionupdate');
+    });
+
+    test('Компонент имеет событие onInput', () => {
+        TEXTFIELD.trigger('input');
+    
+        expect(wrapper.emitted()).toHaveProperty('input');
+    });
+
+    test('Компонент имеет событие onInvalid', () => {
+        TEXTFIELD.trigger('invalid');
+    
+        expect(wrapper.emitted()).toHaveProperty('invalid');
+    });
+
+    test('Компонент имеет событие onReset', () => {
+        TEXTFIELD.trigger('reset');
+    
+        expect(wrapper.emitted()).toHaveProperty('reset');
+    });
+
+    test('Компонент имеет событие onSearch', () => {
+        TEXTFIELD.trigger('search');
+    
+        expect(wrapper.emitted()).toHaveProperty('search');
+    });
+
+    test('Компонент имеет событие onSelect', () => {
+        TEXTFIELD.trigger('select');
+    
+        expect(wrapper.emitted()).toHaveProperty('select');
+    });
+
+    test('Компонент имеет событие onChange', () => {
+        TEXTFIELD.trigger('change');
+    
+        expect(wrapper.emitted()).toHaveProperty('change');
+    });
+
+    test('Компонент имеет событие onCopy', () => {
+        TEXTFIELD.trigger('copy');
+    
+        expect(wrapper.emitted()).toHaveProperty('copy');
+    });
+
+    test('Компонент имеет событие onCut', () => {
+        TEXTFIELD.trigger('cut');
+    
+        expect(wrapper.emitted()).toHaveProperty('cut');
+    });
+
+
 });
-
